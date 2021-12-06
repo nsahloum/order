92fc06c63d7f3bf368eeb08655ad350964ca619a
Najima Sahloumi <nsahloum@gmail.com>
2021-12-06 22:08:25 +0100

    always displaying error message

--- src/main/java/com/switchfully/order/exceptions/UnauthorizedException.java
+++ src/main/java/com/switchfully/order/exceptions/UnauthorizedException.java
@@ -1,7 +1,9 @@
 package com.switchfully.order.exceptions;
 
 public class UnauthorizedException extends RuntimeException{
-    public UnauthorizedException() {this("You are not authorized to access this info. ");}
+    public UnauthorizedException() {
+        this("You are not authorized to access this page");
+    }
 
     public UnauthorizedException(String message) {
         super(message);
