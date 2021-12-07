3d12d60550cd253d2f95874121054548e972c7ee
Najima Sahloumi <nsahloum@gmail.com>
2021-12-07 12:32:32 +0100

    Jenkins test

--- src/test/java/com/switchfully/order/repository/UserRepositoryTest.java
+++ src/test/java/com/switchfully/order/repository/UserRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.switchfully.order.repository;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class UserRepositoryTest {
-    
+    @Test
+    void dummytest(){
+       assertThat("hello").isEqualTo("test");
+    }
 }
